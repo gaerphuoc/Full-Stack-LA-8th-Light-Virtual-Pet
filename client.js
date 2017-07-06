73d24a406aef1e23880da1a286451122ba1e752f
gaerphuoc <gaephuoc@hotmail.com>
2017-07-05 20:13:40 -0700

    Create client.js

---
+++ client.js
@@ -0,0 +1,69 @@
+var pet_info = {
+  "name": "Tomagatchi Dog",
+  "weight": 10,
+  "happiness": 2
+  
+}
+
+$(function() {
+  
+  // Called function to update the name, happiness, and weight of our pet in our HTML
+  checkAndUpdatePetInfoInHtml();
+
+  // When each button is clicked, it will "call" function for that button (functions are below)
+  $('.treat-button').click(clickedTreatButton);
+  $('.play-button').click(clickedPlayButton);
+  $('.exercise-button').click(clickedExerciseButton);
+  
+})
+
+  // Add a variable "pet_info" equal to a dictionary with the name (string), weight (number), and happiness (number) of the pet
+
+  function clickedTreatButton() {
+    // Increase pet happiness
+    pet_info.happiness ++;
+    // Increase pet weight
+    pet_info.weight++;
+    checkAndUpdatePetInfoInHtml();
+  }
+  
+  function clickedPlayButton() {
+    // Increase pet happiness
+    pet_info.happiness++;
+    // Decrease pet weight
+    pet_info.weight--;
+    checkAndUpdatePetInfoInHtml();
+  }
+  
+  function clickedExerciseButton() {
+    // Decrease pet happiness
+    pet_info.happiness--;
+    // Decrease pet weight
+    pet_info.weight--;
+    checkAndUpdatePetInfoInHtml();
+  }
+
+  function checkAndUpdatePetInfoInHtml() {
+    checkWeightAndHappinessBeforeUpdating();  
+    updatePetInfoInHtml();
+  }
+  
+  function checkWeightAndHappinessBeforeUpdating() {
+    //Can't have a weightless dog
+    // Add conditional so if weight is lower than zero, set it back to one
+    if (pet_info.weight<=0){
+      pet_info.weight = 1;
+    }
+    // Add conditional so if happiness is lower than zero, set it back to zero
+    if (pet_info.happiness<=0){
+      pet_info.happiness = 0;
+    }
+    
+  }
+  
+  // Updates your HTML with the current values in your pet_info dictionary
+  function updatePetInfoInHtml() {
+    $('.name').text(pet_info['name']);
+    $('.weight').text(pet_info['weight']);
+    $('.happiness').text(pet_info['happiness']);
+  }
